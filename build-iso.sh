aa2c30b63cd4e38ce4b3a2f9e434d718844f12a1
tzlil <tzlils@protonmail.com>
2021-12-21 18:22:58 +0000

    improved cmake

--- build-iso.sh
+++ build-iso.sh
@@ -9,7 +9,7 @@ function cleanup {
 trap cleanup EXIT
  
 # Copy the relevant files over.
-cp -v build/osdev.elf limine.cfg limine/limine.sys \
+cp -v $1 limine.cfg limine/limine.sys \
 	  limine/limine-cd.bin limine/limine-eltorito-efi.bin $ISO_ROOT/
  
 # Create the bootable ISO.
@@ -17,7 +17,7 @@ xorriso -as mkisofs -b limine-cd.bin \
 		-no-emul-boot -boot-load-size 4 -boot-info-table \
 		--efi-boot limine-eltorito-efi.bin \
 		-efi-boot-part --efi-boot-image --protective-msdos-label \
-		$ISO_ROOT -o image.iso
+		$ISO_ROOT -o $2
  
 # Install Limine stage 1 and 2 for legacy BIOS boot.
-./limine/limine-install image.iso
+./limine/limine-install $2
